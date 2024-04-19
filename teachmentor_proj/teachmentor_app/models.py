6edb25af3d3d45d57d1cd9d714f5070d44a56f34
Narek Chqolyan <159801789+chqoliann@users.noreply.github.com>
2024-04-19 19:13:32 +0400

    Add files via upload

---
+++ teachmentor_proj/teachmentor_app/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Course(models.Model):
+    course_name = models.CharField(max_length=30, blank=False)
+    price = models.IntegerField(default=0)
+    description = models.TextField()
+    likes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.course_name}, {self.price}"
+
+
+class FeedBackMessage(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    email = models.EmailField()
+    message = models.TextField()
+    date_sent = models.DateField()
+
+    def __str__(self):
+        return f"Feedback from {self.name} at {self.date_sent})"
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    registration_date = models.DateField(auto_now_add=True)
+    liked_courses = models.ManyToManyField('Course', related_name='liked_by')
+
+    def __str__(self):
+        return self.user.username
+
