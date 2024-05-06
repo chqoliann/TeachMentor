7f63f3d5d0afa38189c23fc1b4baa4a6919ad08f
Narek Chqolyan <159801789+chqoliann@users.noreply.github.com>
2024-05-06 17:46:09 +0400

    Delete teachmentor_proj directory

--- teachmentor_proj/teachmentor_proj/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('teachmentor_app.urls')),
-    path('', include('teachmentor_ru_app.urls'))
-]
--- teachmentor_proj/teachmentor_ru_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class TeachmentorRuAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'teachmentor_ru_app'
